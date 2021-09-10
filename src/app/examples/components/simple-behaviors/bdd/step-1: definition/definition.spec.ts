d4015b3af62ce064e69966d45eb108e8561c939a
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-10 17:34:51 -0400

    added tests

--- src/app/examples/components/simple-behaviors/bdd/step-1: definition/definition.spec.ts
+++ src/app/examples/components/simple-behaviors/bdd/step-1: definition/definition.spec.ts
@@ -1,9 +1,23 @@
 describe('simple behaviors component', () => {
-    describe('Given: initialization completed', () => {
-        describe('When: rendered', () => {
-            it('Then: should display the title', () => {
-                expect(true).toBeTrue();
-            })
-        })
-    })
-})
+  describe('Given: initialization completed', () => {
+    describe('When: rendered', () => {
+      it('Then: should display the title', () => {
+        expect(true).toBeTrue();
+      });
+
+      it('Then: should display a button with the label="increment"', () => {
+        expect(true).toBeTrue();
+      });
+
+      it('Then: should display a counter that is initialized to zero', () => {
+        expect(true).toBeTrue();
+      });
+
+      describe('When: the increment-button is clicked', () => {
+          it ('Then: the counter should be incremented', () => {
+              expect(true).toBeTrue();
+          })
+      })
+    });
+  });
+});
