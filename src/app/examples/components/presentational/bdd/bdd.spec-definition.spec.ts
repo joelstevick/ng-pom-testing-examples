c276d7b739ef99870ce1502bb3d9397e24f66cd4
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-10 14:50:22 -0400

    completed spec for spec

---
+++ src/app/examples/components/presentational/bdd/bdd.spec-definition.spec.ts
@@ -0,0 +1,8 @@
+describe('presentational component', () => {
+    describe('Given: initialization completed', () => {
+        describe('When: rendered', () => {
+            it('Then: should display the title', () => {
+            })
+        })
+    })
+})
--- src/app/examples/components/presentational/presentational.component.ts
+++ src/app/examples/components/presentational/presentational.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-presentational',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresentationalComponent implements OnInit {
 
+  @Input()
+  title: string;
+  
   constructor() { }
 
   ngOnInit(): void {
