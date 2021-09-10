87b06161ba5be2ac05567a39d0f2bdc1fd05d965
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-10 14:52:30 -0400

    reorganized

--- src/app/examples/components/presentational/presentational.component.ts
+++ src/app/examples/components/presentational/presentational.component.ts
@@ -7,9 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PresentationalComponent implements OnInit {
 
-  @Input()
-  title: string;
-  
   constructor() { }
 
   ngOnInit(): void {
