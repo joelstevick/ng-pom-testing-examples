e36057e57a3d8d56ac35f2d25608a5a1f83295a0
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-11 14:02:56 -0400

    progress control

---
+++ src/app/examples/components/counter/user-story.3.need-progress-control-when-counter-greater-than-zero/step-2: implementation/implementation.spec.ts
@@ -0,0 +1,74 @@
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { nanoid } from 'nanoid';
+import { POM } from 'ng-pom-testing';
+import { CounterComponent } from '../../counter.component';
+import { counterPomConfig } from './counter.angular-testbed.pom';
+
+describe('Counter component', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+  let compiled: any;
+  let pom: POM;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CounterComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    compiled = fixture.nativeElement;
+    pom = new POM({ fixture, component, compiled }, counterPomConfig);
+
+    fixture.detectChanges();
+  });
+
+  describe('Given: initialization completed', () => {
+    describe('When: rendered', () => {
+
+      it('Then: should display a button where the label is bound to the "label" component property', () => {
+       // arrange
+       const label = nanoid();
+
+       // act
+       pom.action('set-label', label);
+
+       // assert
+       expect(compiled.querySelector('[data-testid=increment-btn]').textContent).toContain(label);
+      });
+
+      it('Then: should display a counter that is initialized to zero', () => {
+      
+        // arrange -- defaults to 0
+
+        // act -- testing initial state
+
+        // assert
+        expect(
+          compiled.querySelector('[data-testid=counter]').textContent
+        ).toContain(fixture.componentInstance.counter);
+      });
+
+      describe('When: the increment-button is clicked', () => {
+        it('Then: the counter should be incremented', fakeAsync(() => {
+          // arrange 
+          const counter = compiled.querySelector('[data-testid=counter]');
+          const counterBefore = fixture.componentInstance.counter;
+
+          // act
+          pom.action('click-increment-btn');
+
+          // assert
+          expect(counter.textContent).toContain(counterBefore + 1);
+        }));
+      });
+    });
+  });
+});
