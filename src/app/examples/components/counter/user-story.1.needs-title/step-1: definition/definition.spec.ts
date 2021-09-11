cc3d41d4e6c348c168bcba04091e43d0559baed0
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-11 09:00:42 -0400

    major refactor to focus on bdd sdlc against a single component

--- src/app/examples/components/counter/user-story.1.needs-title/step-1: definition/definition.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('presentational component', () => {
-    describe('Given: initialization completed', () => {
-        describe('When: rendered', () => {
-            it('Then: should display the title', () => {
-                expect(true).toBeTrue();
-            })
-        })
-    })
-})
