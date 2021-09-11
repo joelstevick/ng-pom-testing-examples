ac7d36bf65177ed6a3140b7755af81636d8c05bf
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-11 14:00:40 -0400

    progress control

---
+++ src/app/examples/components/counter/user-story.3.need-progress-control-when-counter-greater-than-zero/step-1: definition/definition.spec.ts
@@ -0,0 +1,20 @@
+describe('Counter component', () => {
+  describe('Given: initialization completed', () => {
+    describe('When: rendered', () => {
+
+      it('Then: should not display the progress control', () => {
+        expect(true).toBeTrue();
+      });
+
+
+      describe('When: the increment-button has been clicked', () => {
+        it('Then: the progress control should be displayed', () => {
+          expect(true).toBeTrue();
+        });
+        it('Then: the progress control value should match the counter', () => {
+          expect(true).toBeTrue();
+        });
+      });
+    });
+  });
+});
