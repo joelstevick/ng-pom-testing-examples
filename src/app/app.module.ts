eb5c0729b23a81ada615f0530d8806eaccedd699
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-10-29 14:37:01 -0400

    added parent <- child <- grand-child example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './examples/components/counter/counter.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { ParentComponent } from './examples/components/parent/parent.component';
+import { ChildComponent } from './examples/components/parent/child/child.component';
+import { GrandChildComponent } from './examples/components/parent/child/grand-child/grand-child.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
+  declarations: [AppComponent, CounterComponent, ParentComponent, ChildComponent, GrandChildComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatProgressBarModule],
   providers: [],
   bootstrap: [AppComponent],
