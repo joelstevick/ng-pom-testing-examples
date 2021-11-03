654373c50937ced2ab2cda18243bea5b32dc6f22
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-11-03 12:54:47 -0400

    interactions with mat-slider-toggle work via Angular Testbed

--- src/app/examples/components/mat-slide-toggle/mat-slide-toggle.component.spec.ts
+++ src/app/examples/components/mat-slide-toggle/mat-slide-toggle.component.spec.ts
@@ -1,25 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { By } from '@angular/platform-browser';
 import { MatSlideToggleComponent } from './mat-slide-toggle.component';
 
-describe('MatSlideToggleComponent', () => {
+fdescribe('MatSlideToggleComponent.', () => {
   let component: MatSlideToggleComponent;
   let fixture: ComponentFixture<MatSlideToggleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MatSlideToggleComponent ]
+      declarations: [ MatSlideToggleComponent ],
+      imports: [MatSlideToggleModule]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MatSlideToggleComponent);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', fakeAsync(() => {
     expect(component).toBeTruthy();
-  });
+
+    const checkbox = fixture.debugElement.nativeElement.querySelectorAll('mat-slide-toggle input')[0];
+
+    checkbox.click();
+
+    fixture.detectChanges();
+
+    console.log(fixture.debugElement.nativeElement.querySelectorAll('mat-slide-toggle')[0])
+
+
+  }));
 });
